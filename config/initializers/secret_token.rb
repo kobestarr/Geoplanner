4c7ca653b682e29fdd0c0e75e5efff7d68c93424
Kobi Omenaka <kobestarr@Kobestarrs-RetinaMBP.local>
2014-01-22 15:02:26 +0000

    Added rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geoplanner::Application.config.secret_key_base = '092764a3cf37cd54133c55d64c2a8c6a7cecf92b3837c19013299bcd8065440c051e0d74521c008b8d262b18e6e2e22761379d51acbdc9b676022e1b3fc39e5a'
